5128261bfe6be3037a318ebd4763c9675b879a6f
Liisa Maanavilja <liisa.maanavilja@hsl.fi>
2017-12-14 14:12:47 +0200

    Lisään muutetut tiedostot

--- joukko/R/lisaaAlkio.R
+++ joukko/R/lisaaAlkio.R
@@ -1,3 +1,12 @@
+#' Lisää alkio
+#'
+#' @param joukko
+#' @param alkio
+#'
+#' @return joukon ja siihen lisätyn alkion
+#' @export
+#'
+#' @examples
 lisaaAlkio <- function(joukko, alkio) {
   return(unique(c(joukko, alkio)))
 }
