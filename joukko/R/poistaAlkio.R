7472333b5706ec943e35735ee3d17305d316f069
hannakitti <hanna.kitti@outlook.com>
2017-12-14 15:05:25 +0200

    lisaa testi poistaAlkio.R

--- joukko/R/poistaAlkio.R
+++ joukko/R/poistaAlkio.R
@@ -1,3 +1,12 @@
+#' Title Poistaa alkion
+#'
+#' @param joukko joukko alkioita
+#' @param alkio poista alkio
+#'
+#' @return joukko, josta alkio poistetaan
+#' @export
+#'
+#' @examples
 poistaAlkio <- function(joukko, alkio) {
   if (alkio %in% joukko) {
     joukko <- joukko[joukko != alkio]
---
+++ joukko/tests/testthat/test_poistaJoukko.R
@@ -0,0 +1,3 @@
+test_that("poistaAlkio works as expected", {
+  expect_equal(poistaAlkio("x", "y"), "x")
+})
